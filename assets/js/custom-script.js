b13033be894983fd7bbaf083ea14d1047b6df632
boulevard-of-broken-dreams <dash.sourav126@gmail.com>
2023-08-06 11:41:09 +0530

    fix: contact form validation & Nav menu highlighting

--- assets/js/custom-script.js
+++ assets/js/custom-script.js
@@ -22,32 +22,81 @@ $(document).ready(function () {
   }
 
   var contactForm = $("#submit-now");
-  contactForm.click(function () {
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:3000/contact",
-      data: JSON.stringify({
-        name: $("#name").val(),
-        phone: $("#phone").val(),
-        email: $("#email").val(),
-        message: $("#comments").val(),
-      }), // now data come in this function
-      contentType: "application/json; charset=utf-8",
-      crossDomain: true,
-      dataType: "json",
-      success: function (data, status, jqXHR) {
-        alert("Your response recorded successfully.");
-        $("#name").val("");
-        $("#phone").val("");
-        $("#email").val("");
-        $("#comments").val("");
-      },
-      error: function (jqXHR, status) {
-        // error handler
-        console.log(jqXHR);
-        alert("fail" + status.code);
-      },
-    });
+  contactForm.click(function (event) {
+    event.preventDefault();
+    var nameInput = $("#name");
+    var phoneInput = $("#phone");
+    var emailInput = $("#email");
+    var isNameValid = false;
+    var isPhoneValid = false;
+    var isEmailValid = false;
+    var formData = document.getElementById("contact-form");
+    if ((nameInput.val() || "").length <= 0) {
+      nameInput.attr("class", "form-control input-fill invalid-input");
+    } else {
+      isNameValid = true;
+      nameInput.attr("class", "form-control input-fill");
+    }
+
+    if ((phoneInput.val() || "") <= 0) {
+      phoneInput.attr("class", "form-control input-fill invalid-input");
+    } else {
+      if (/^(\+91[\-\s]?)?(?:\d{10})$/.test(phoneInput.val())) {
+        isPhoneValid = true;
+        phoneInput.attr("class", "form-control input-fill");
+      } else {
+        phoneInput.attr("class", "form-control input-fill invalid-input");
+      }
+    }
+
+    if ((emailInput.val() || "") <= 0) {
+      emailInput.attr("class", "form-control input-fill invalid-input");
+    } else {
+      if (
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+          emailInput.val()
+        )
+      ) {
+        isEmailValid = true;
+        emailInput.attr("class", "form-control input-fill");
+      } else {
+        emailInput.attr("class", "form-control input-fill invalid-input");
+      }
+    }
+
+    if (isNameValid && isPhoneValid && isEmailValid) {
+      $("#form-spinner").show();
+      $.ajax({
+        type: formData.method,
+        url: formData.action,
+        data: JSON.stringify({
+          name: $("#name").val(),
+          phone: $("#phone").val(),
+          email: $("#email").val(),
+          message: $("#comments").val(),
+        }), // now data come in this function
+        contentType: "application/json; charset=utf-8",
+        crossDomain: true,
+        dataType: "json",
+        success: function (data, status, jqXHR) {
+          $("#form-spinner").hide();
+          $("#my-form-status").html("Thanks for your submission!");
+          setTimeout(() => {
+            $("#my-form-status").html("");
+          }, 3000);
+          $("#name").val("");
+          $("#phone").val("");
+          $("#email").val("");
+          $("#comments").val("");
+        },
+        error: function (jqXHR, status) {
+          $("#form-spinner").hide();
+          // error handler
+          console.log(jqXHR);
+          alert("fail" + status.code);
+        },
+      });
+    }
   });
 });
 
@@ -132,3 +181,59 @@ window.ityped.init(document.querySelector(".faq-section .ityped"), {
   backSpeed: 150,
   loop: true,
 });
+
+// var form = document.getElementById("contact-form");
+
+// async function handleSubmit(event) {
+//   event.preventDefault();
+//   const nameInput = document.getElementById("name");
+//   const phoneInput = document.getElementById("phone").value;
+//   const emailInput = document.getElementById("email").value;
+//   if (nameInput.value === "") {
+//     nameInput.setAttribute("class", "invalid-input");
+//   } else {
+//     nameInput.removeAttribute("invalid-input");
+//   }
+//   if (phoneInput.value === "") {
+//     phoneInput.setAttribute("class", "invalid-input");
+//   } else {
+//     phoneInput.removeAttribute("invalid-input");
+//   }
+//   if (emailInput.value === "") {
+//     emailInput.setAttribute("class", "invalid-input");
+//   } else {
+//     emailInput.removeAttribute("invalid-input");
+//   }
+
+//   console.log(data);
+//   console.log("event.target");
+
+//   // var status = document.getElementById("my-form-status");
+//   // fetch(event.target.action, {
+//   //   method: form.method,
+//   //   body: data,
+//   //   headers: {
+//   //     Accept: "application/json",
+//   //   },
+//   // })
+//   //   .then((response) => {
+//   //     if (response.ok) {
+//   //       status.innerHTML = "Thanks for your submission!";
+//   //       form.reset();
+//   //     } else {
+//   //       response.json().then((data) => {
+//   //         if (Object.hasOwn(data, "errors")) {
+//   //           status.innerHTML = data["errors"]
+//   //             .map((error) => error["message"])
+//   //             .join(", ");
+//   //         } else {
+//   //           status.innerHTML = "Oops! There was a problem submitting your form";
+//   //         }
+//   //       });
+//   //     }
+//   //   })
+//   //   .catch((error) => {
+//   //     status.innerHTML = "Oops! There was a problem submitting your form";
+//   //   });
+// }
+// form.addEventListener("submit", handleSubmit);
