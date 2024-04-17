af7f137991722fc8642651372e77c641d2ca78c6
boulevard-of-broken-dreams <dash.sourav126@gmail.com>
2024-04-17 10:19:22 +0530

    added new section in digifact

--- assets/js/digifact.js
+++ assets/js/digifact.js
@@ -15,4 +15,15 @@ $(document).ready(function () {
     offset: 120,
     easing: "ease-in-out",
   });
+
+  $("#data-extraction-btn").trigger("click");
+
+  $("#usecases-details").on("show.bs.collapse", function (e) {
+    var $this = $(e.target);
+    $(".highlighted").each(function () {
+      $(this).removeClass("highlighted");
+    });
+    $("#" + $this.attr("id") + "-btn").addClass("highlighted");
+    $("#usecases-details .collapse").not($this).collapse("hide");
+  });
 });
